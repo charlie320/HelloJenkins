39be1d6dce792c31edf4a00c0ed68351e04809cf
Charlie de la Rosa <jcdelarosa@verizon.net>
2020-03-03 17:27:03 -0600

    Added string methods.

--- QEModule_Jenkins_Hello/src/main/java/com/perscholas/jenkins_hello/HelloJenkins.java
+++ QEModule_Jenkins_Hello/src/main/java/com/perscholas/jenkins_hello/HelloJenkins.java
@@ -3,8 +3,16 @@ package com.perscholas.jenkins_hello;
 public class HelloJenkins {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Jenkins!");
-		System.out.println("Hello Jenkins again!!!");
+		System.out.println(helloJenkins());
+		System.out.println(helloJenkinsAgain());
+	}
+	
+	public static String helloJenkins() {
+		return "Hello Jenkins!";
+	}
+	
+	public static String helloJenkinsAgain() {
+		return "Hello Jenkins again!!!";
 	}
 
 }
